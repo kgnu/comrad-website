ce21304888fadfce99c32712beff605d71b43312
seankelsonwilliams@gmail.com <seankelsonwilliams@gmail.com@fd41c1d1-8702-e1e8-af49-bb656cc03180>
2011-05-07 23:53:14 +0000

    update to scripts - added logging and server paths for launch --This line, and those below, will be ignored--
    M    archiving/archiveShows.php
    M    archiving/config.php
    M    archiving/getUpcomingEvents.php

--- trunk/archiving/archiveShows.php
+++ trunk/archiving/archiveShows.php
@@ -7,14 +7,20 @@
 	
 	//this script uses StreamRipper - http://manpages.ubuntu.com/manpages/dapper/man1/streamripper.1.html
 	
+	//set a script timeout of one day
+	set_time_limit(24 * 60 * 60);
+	
+	//include the configuration and the log file
+	require('config.php');
+	require('log.php');
+	
 	//this error handler will write out any PHP errors
 	set_error_handler('errorHandler');
 	function errorHandler($errno, $errstr, $errfile, $errline) {
-		echo '<br /><b>Error</b>: ' . $errstr . ' (line '.$errline.' in '.$errfile.')<br />';
+		logText('PHP Error: ' . $errstr . ' (line '.$errline.' in '.$errfile.')');
 	}
 	
-	//include the configuration file
-	require('config.php');
+	logText('Starting executing archiveShows.php');
 	
 	//open the file of upcoming events
 	if (!file_exists($upcomingEventsFile)) {
@@ -33,6 +39,10 @@
 			$event["recordingStartTime"] = $v[1];
 			$event["endTime"] = $v[2];
 			$event["startTime"] = $v[3];
+			$event["titleWithSpaces"] = $v[4];
+			$event["host"] = $v[5];
+			$event["eventId"] = $v[6];
+			$event["hostId"] = $v[7];
 			if (time() >= (int)$event["recordingStartTime"] && time() <= (int)$event["endTime"]) { //the event is active if the current time is after the start time, but before the end time
 				$activeEvent = true;
 				break;
@@ -42,55 +52,141 @@
 	
 	//if we didn't find an active event, abandon the script
 	if (!$activeEvent) {
-		echo 'no active event, exiting script.<br />';
+		logText('no active event, exiting script.');
 		exit();
 	} else {
-		echo 'active event, trying to begin recording...<br />';
+		logText('active event, trying to begin recording...');
 	}
 	
 	//make a new directory for the show ...streamripper will automatically create the directory
-	$folder = date("m-d-y_Gi",$event["recordingStartTime"])."_".$event["title"];
+	$folder = $event["title"]."_".date("Y-m-d",$event["recordingStartTime"]);
 	$folder = str_replace(" ","",$folder); //remove spaces from the folder name
-	if (file_exists($destination.$folder."/stream.mp3")) {
+	if (file_exists($temporaryDestination.$folder."/stream.mp3")) {
 		//file already exists - the script is already running
 		//TODO: we should check to see if the file is locked, and restart recording if it's not. if the file is not locked, we could assume that streamripper
 		//has stopped for some reason
-		echo 'stream is already being recorded at '.$destination.$folder.'/stream.mp3...exiting script.';
+		logText('stream is already being recorded at '.$destination.$folder.'/stream.mp3...exiting script.');
 		exit();
 	}
 	$command = "streamripper ".$stream; //setup the terminal command
-	$command .= " -d ".$destination.$folder; //set the destination directory
+	$command .= " -d ".$temporaryDestination.$folder; //set the destination directory
 	$command .= " -a stream"; //record to one file - we don't want to separate the stream into tracks
 	$command .= " -m ".$timeout; //reset stream connection after a 60 second timeout
 	//get the duration of the file
 	$duration = (int)$event["endTime"] - (int)$event["recordingStartTime"] + $extraRecordingTime;
 	$command .= " -l ".($duration);
 	$command .= " --debug"; //save a debugging log
-	$command .= " > /dev/null &"; //run asynchronously so the script finishes - got this from http://www.sitecrafting.com/blog/to-run-php-code-in/
+	//sw commented out 5/4/11 - we decided to run this syncrhonously, then monitor the return value
+	//$command .= " > /dev/null &"; //run asynchronously so the script finishes - got this from http://www.sitecrafting.com/blog/to-run-php-code-in/
 	
-	echo 'launching streamripper: '.$command."<br />";;
-	exec($command);
+	logText('launching streamripper with command: '.$command);
+	exec($command, $output, $returnStatus);
 	
-	//update the database with the recording path
+	if ($returnStatus == "0") {
+		logText('streamripper successful - moving file to permanent location');
+		//successful execution
+		//move the file to its permanent location
+		//create any folders that are not created yet
+		if (file_exists($destination.$event["title"])) {
+			mkdir($destination.$event["title"]);
+		}
+		if (file_exists($destination.$event["title"]."/".date("Y", $event["recordingStartTime"]))) {
+			mkdir($destination.$event["title"]."/".date("Y", $event["recordingStartTime"]));
+		}
+		//move the file to its permanent location
+		$recordedFileName = $destination.$event["title"]."/".date("Y", $event["recordingStartTime"])."/".$event["title"]."_".date("Y-m-d").".mp3";
+		rename($temporaryDestination.$folder."/stream.mp3", $recordedFileName);
+		
+		//delete the temporary file
+		unlinkRecursive($temporaryDestination.$folder, true);
 	
-	//initialize classes that will let us access the database
-	require_once($root.'lib/classes/Initialize.php');
-	$init = new Initialize();
-	$init->setAutoload();
-	
-	//disable authorization
-	$pm = PermissionManager::getInstance();
-	$pm->disableAuthorization();
-	
-	//get all events in the next day
-	$em = EventManager::getInstance();
-	$eventInstance = $em->getEventsBetween($event["startTime"], $event["endTime"]);
-	
-	foreach ($eventInstance as $ei) {
-		$ei->RecordedFileName = $destination.$folder."/stream.mp3";
+		//update the database with the recording path
+		logText('file moved to permanent location - updating the database with the file path');
+		
+		//initialize classes that will let us access the database
+		require_once($root.'lib/classes/Initialize.php');
+		$init = new Initialize();
+		$init->setAutoload();
+		
+		//disable authorization
+		$pm = PermissionManager::getInstance();
+		$pm->disableAuthorization();
+		
+		//get all events in the next day
+		$em = EventManager::getInstance();
+		$eventInstance = $em->getEventsBetween($event["startTime"], $event["endTime"]);
+		
+		foreach ($eventInstance as $ei) {
+			$ei->RecordedFileName = $httpDestination.$event["title"]."/".date("Y", $event["recordingStartTime"])."/".$event["title"]."_".date("Y-m-d").".mp3";
+		}
+		
+		
+		//write ID3 tags using EYED3
+		//http://eyed3.nicfit.net/
+		$command = "eyeD3";
+		$command .= ' -a "'.$event["host"].'"'; //artist - here, we're using the host
+		$command .= ' -A "'.$event["titleWithSpaces"].'"'; // album - we'll use the show title
+		$command .= ' -t "'.$event["titleWithSpaces"].'"'; //title - we'll use the show title
+		$command .= ' -Y '.date("Y", $event["recordingStartTime"]); //year
+		$command .= ' -p KGNU'; //publisher
+		$command .= ' file '.$recordedFileName;
+		logText('writing ID3 tags with commad: '.$command);
+		exec($command, $output, $returnStatus);
+		if ($returnStatus == "0") {
+			logText("successfully wrote ID3 tags");
+		} else {
+			logText("error writing ID3 tags");
+		}
+		
+		//write RSS feed for podcasting
+		require('createPodcasts.php');
+		writeRssFeedForHost($event["hostId"]);
+		writeRssFeedForShow($event["showId"]);
+		
+	} else {
+		//unsuccessful execution
+		logText("streamripper returned an unsuccessful status code");
 	}
+	
+	truncateLog();
 
 	//TODO: let the script start if it's running late...possibly, just by checking if the file's there. it should also resume if it unexpectedly restarts (the resume functionality can be deferred)
 	
+	
+	
+	//code from http://fr2.php.net/manual/en/function.unlink.php
+	/**
+	 * Recursively delete a directory
+	 *
+	 * @param string $dir Directory name
+	 * @param boolean $deleteRootToo Delete specified top-level directory as well
+	 */
+	function unlinkRecursive($dir, $deleteRootToo)
+	{
+	    if(!$dh = @opendir($dir))
+	    {
+		return;
+	    }
+	    while (false !== ($obj = readdir($dh)))
+	    {
+		if($obj == '.' || $obj == '..')
+		{
+		    continue;
+		}
 
+		if (!@unlink($dir . '/' . $obj))
+		{
+		    unlinkRecursive($dir.'/'.$obj, true);
+		}
+	    }
+
+	    closedir($dh);
+	   
+	    if ($deleteRootToo)
+	    {
+		@rmdir($dir);
+	    }
+	   
+	    return;
+	} 
 ?>
--- trunk/archiving/config.php
+++ trunk/archiving/config.php
@@ -5,14 +5,32 @@
 	//the path to the root of the comrad-dev folder
 	$root = "../../../"; 
 	
+	//the path of the log file to store all script output
+	$logFile = "/srv2/www/kgnu/audioarchives/archivingLog.txt";
+	
+	//the maximum number of lines for the log file
+	$logFileMaxLines = 10000;
+	
 	//this file is a text file that stores text data of the upcoming events
-	$upcomingEventsFile = "/var/www/comrad-dev-read-only/lib/utilities/archiving/upcomingEvents.txt"; 
+	$upcomingEventsFile = "/srv2/www/kgnu/audioarchives/upcomingEvents.txt"; 
 	
 	//the stream URL to record - do not include the http:// part of the URL, as this causes a hostname can't be resolved error
 	$stream = 'stream.kgnu.net:8000/KGNU_live_high.mp3.m3u'; 
 	
-	//the destination where the MP3 file of the stream will be written
-	$destination = "/var/www/comrad-dev-read-only/lib/utilities/archiving/kgnu-archives/";
+	//the destination where the MP3 file of the stream will be stored
+	//the folder structure looks like:
+	// $destination . "(show title) / (year) / ShowName_Year-Month-Date.mp3"
+	$destination = "/srv2/www/kgnu/audioarchives/";
+	
+	//the HTTP address to the destination folder specified above:
+	$httpDestination = "http://kgnu.net/audioarchives/";
+	
+	//the destination folder to store RSS feeds
+	$rssFilePath = "/srv2/www/kgnu/audioarchives"; 
+	
+	//the temporary recording location streamripper will use to record the file - after the file is done recording, it
+	//will move the file to the permanent destination specified above
+	$temporaryDestination = "/srv2/www/kgnu/audioarchives/temp/";
 	
 	//the extra time, in seconds, to record for
 	//For example, if Afternoon Sound Alternative ends at 3:00 PM and this value is set to 5 * 60 seconds, the recording will end at 3:05 PM.
