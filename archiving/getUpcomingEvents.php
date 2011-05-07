ca8ceaf83228ec78c8e8925b30c9599a5b81b9e0
seankelsonwilliams@gmail.com <seankelsonwilliams@gmail.com@fd41c1d1-8702-e1e8-af49-bb656cc03180>
2011-05-07 23:53:14 +0000

    update to scripts - added logging and server paths for launch --This line, and those below, will be ignored--
    M    archiving/archiveShows.php
    M    archiving/config.php
    M    archiving/getUpcomingEvents.php

--- archiving/getUpcomingEvents.php
+++ archiving/getUpcomingEvents.php
@@ -1,18 +1,24 @@
 <?php
 
 	//created sean williams 4/4/11
-	//to be run every 30 minutes
+	//to be run every hour
 	//reads upcoming shows from the database and writes them out to a text file
 	//writes info of shows to be recorded into a text file
+	
+	//set a script timeout of thirty minutes
+	set_time_limit(30 * 60);
+	
+	//include the configuration and log file
+	require('config.php');
+	require('log.php');
 	
 	//this error handler will write out any PHP errors
 	set_error_handler('errorHandler');
 	function errorHandler($errno, $errstr, $errfile, $errline) {
-		echo '<br /><b>Error</b>: ' . $errstr . ' (line '.$errline.' in '.$errfile.')<br />';
+		logText('PHP Error: ' . $errstr . ' (line '.$errline.' in '.$errfile.')');
 	}
 	
-	//include the configuration file
-	require('config.php');
+	logText('Started executing getUpcomingEvents.php');
 	
 	//initialize classes that will let us access the database
 	require_once($root.'lib/classes/Initialize.php');
@@ -35,16 +41,24 @@
 		$event = $scheduledEvent->Event;
 		//if we're supposed to record the event, store the event in the text file
 		if ($event->RecordAudio) {
+			$title = preg_replace("/[^A-Za-z0-9]/","",$event->Title); //strip all non-alphanumeric characters out of the title, since this will be used for a file/folder name
 			$startTime = $scheduledEventInstance->StartDateTime;
 			$recordingStartTime = $startTime + ($scheduledEventInstance->ScheduledEvent->RecordingOffset * 60);
 			$endTime = $startTime + ($scheduledEventInstance->Duration * 60);
-			$upcomingEvents .= $event->Title . "|" . $recordingStartTime  . "|" . $endTime .  "|" . $startTime . "\n";
+			$upcomingEvents .= $title . "|" . $recordingStartTime  . "|" . $endTime .  "|" . $startTime . "|" . $event->Title . "|" . 
+							 $event->Host . "|" . $event->EventId . "|" . $event->HostId . "\n";
 		}
 	}
+	
 	
 	//write out the upcoming events to a file
 	$fh = fopen($upcomingEventsFile,'w');
 	fwrite($fh,$upcomingEvents);
 	fclose($fh);
+	
+	logText('Finished executing getUpcomingEvents.php');
+	
+	truncateLog();
+
 	
 ?>
